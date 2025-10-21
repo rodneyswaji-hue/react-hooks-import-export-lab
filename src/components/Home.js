d35efbd00b141bb0ef6a32f1f54289be2e28e3b8
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-10-21 10:02:25 +0300

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const username = "Rodney";
+const city = "Nashville";
 function Home() {
   return (
     <div id="home">
@@ -9,3 +11,4 @@ function Home() {
     </div>
   );
 }
+export default Home;
